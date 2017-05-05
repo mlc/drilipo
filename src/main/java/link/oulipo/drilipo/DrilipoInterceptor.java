b94a530b5c56c2fe1cf52285387dcadaf11088ea
mike castleman <m@mlcastle.net>
2017-05-05 18:52:14 -0400

    put okhttp info in UA

--- src/main/java/link/oulipo/drilipo/DrilipoInterceptor.java
+++ src/main/java/link/oulipo/drilipo/DrilipoInterceptor.java
@@ -7,10 +7,11 @@ import okhttp3.Response;
 import java.io.IOException;
 
 public class DrilipoInterceptor implements Interceptor {
+    private final String ua = "drilipo/1.0 " + okhttp3.internal.Version.userAgent();
     @Override
     public Response intercept(Chain chain) throws IOException {
         return chain.proceed(chain.request().newBuilder()
-                .header("User-Agent", "drilipo/1.0")
+                .header("User-Agent", ua)
                 .addHeader(HttpHeaders.ACCEPT_CHARSET, "utf-8")
                 .build());
     }
